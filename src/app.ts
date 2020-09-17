571334c80cbd2ca26cf842f7e412f581208c4424
Eniola Olaniyan <eni052@Enis-MacBook-Pro.local>
2020-09-17 16:19:00 -0400

    set up mong driver

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import * as http from "http";
 import { ExpressDriver } from './drivers/express/express';
+import { MongoDriver } from "./drivers/mongo/mongo";
+require("dotenv").config();
 
 const app = ExpressDriver.build();
 const server = http.createServer(app)
+const connectionString = process.env.dbURI;
 const PORT = 3000;
 
 server.listen(PORT, () => {
-    console.log(`Social Media backend is running on ${PORT}`)
+    console.log(`Social Media backend is running on ${PORT}`);
+});
+MongoDriver.build(connectionString).then( () => {
+    console.log('Connection to the database was successful 🟢');
 });
---
+++ src/drivers/mongo/mongo.ts
@@ -0,0 +1,26 @@
+import { MongoClient, Db } from "mongodb";
+
+export class MongoDriver {
+    static db: Db;
+    static mongoClient: MongoClient;
+
+    private constructor() {}
+
+    private async connect(dbURI: string, dbName?: string): Promise<void> {
+        try {
+            MongoDriver.mongoClient = await MongoClient.connect(dbURI);
+            MongoDriver.db = MongoDriver.mongoClient.db(dbName);
+        } catch (error) {
+            return Promise.reject('There was a problem connecting to the Mongodb')
+        }
+    }
+
+    static async build(dbUri: any): Promise<void> {
+        if(!MongoDriver.mongoClient) {
+            const driver = new MongoDriver();
+            await driver.connect(dbUri);
+        } else {
+            return Promise.reject ('There already is an instance running');
+        }
+    }
+}
